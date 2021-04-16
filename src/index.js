4029b142664bd85345087334142121cfde3a7564
MajedBannouri <majed.bannouri12@gmail.com>
2021-04-16 22:57:58 +0100

    graphql types definition

--- src/index.js
+++ src/index.js
@@ -21,28 +21,24 @@ const books = [
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    avatar: String
   }
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+  type TaskList {
+    id: ID!
+    createdAt: String!
+    title: String!
+    progress: Float!
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+    users: [User!]!
+    # todos
   }
 `;
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+const resolvers = {};
 
 const start = async () => {
   // DB connection
@@ -52,12 +48,10 @@ const start = async () => {
   });
   await client.connect();
   const db = client.db(DB_NAME);
+  const context = { db };
 
-  // The ApolloServer constructor requires two parameters: your schema
-  // definition and your set of resolvers.
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({ typeDefs, resolvers, context });
 
-  // The `listen` method launches a web server.
   server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
